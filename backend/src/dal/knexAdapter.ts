0be16bfa5d1274b60289220dc499828a88e71833
Jessy Anglehart-Nunes <ges13596@gmail.com>
2019-02-16 20:37:24 -0500

    fixed up a bad commit

--- backend/src/dal/knexAdapter.ts
+++ backend/src/dal/knexAdapter.ts
@@ -1,15 +1,12 @@
 import knex from "knex";
+import KnexConfig from "../config/knexConfig";
 
 export default class KnexAdapter {
   static setUp = () => {
+    
     knex({
       client: 'pg',
-      connection: {
-        host : '127.0.0.1',
-        user : 'your_database_user',
-        password : 'your_database_password',
-        database : 'myapp_test'
-      }
+      connection: KnexConfig,
     });
   }
 }
