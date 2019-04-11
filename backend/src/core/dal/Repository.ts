ba66b227a7df6ac3e91ad1d3e32393c9495a5513
Jessy Anglehart-Nunes <ges13596@gmail.com>
2019-04-11 17:45:55 -0400

    adapted repository

--- backend/src/core/dal/Repository.ts
+++ backend/src/core/dal/Repository.ts
@@ -1,30 +1,30 @@
-import Knex, { QueryBuilder } from "knex";
-import Id from "../models/Id";
-import IRepository from "./IRepository";
-import KnexWrapper from "./KnexWrapper";
+import Knex, { QueryBuilder } from 'knex';
+import Id from '../models/Id';
+import IRepository from './IRepository';
+import KnexWrapper from './KnexWrapper';
 
 export enum orderBy {
   desc = 'desc',
-  asc = 'asc',
+  asc = 'asc'
 }
 
 export interface IOrder {
-  col : string;
-  orderBy : orderBy;
+  col: string;
+  orderBy: orderBy;
 }
 
 export default class Repository<T extends Id> implements IRepository<T> {
-  private db : QueryBuilder;
+  private db: QueryBuilder;
 
-  constructor(tableName : string) {
+  constructor(tableName: string) {
     this.db = KnexWrapper.getInstance().knex(tableName);
   }
 
-  public async find(id : number) : Promise<T> {
-    return this.db.where({id,}).first();
+  public async find(id: number): Promise<T> {
+    return this.db.where({ id }).first();
   }
 
-  public async get(filter?: any, order?: IOrder) : Promise<T[]> {
+  public async get(filter?: any, order?: IOrder): Promise<T[]> {
     let query = this.db.where(filter || {});
     if (order) {
       query = query.orderBy(order.col, order.orderBy);
@@ -32,15 +32,15 @@ export default class Repository<T extends Id> implements IRepository<T> {
     return query;
   }
 
-  public add(entity : T) : void {
-    this.db.insert(entity);
+  public async add(entity: T): Promise<any> {
+    await this.db.insert({ ...entity });
   }
 
-  public delete(id : number) : void {
-    this.db.where({id,}).del();
+  public delete(id: number): void {
+    this.db.where({ id }).del();
   }
 
-  public update(entity : T) : void {
+  public update(entity: T): void {
     this.db.where(entity.id).update(entity);
   }
 }
--- backend/src/core/dal/knexAdapter.ts
+++ backend/src/core/dal/knexAdapter.ts
@@ -1,12 +1,11 @@
-import knex from "knex";
-import KnexConfig from "../../config/knexConfig";
+import knex from 'knex';
+import KnexConfig from '../../config/knexConfig';
 
 export default class KnexAdapter {
-  public static setUp = () => {
-    
-    return knex({
+  public static setUp = () =>
+    knex({
       client: 'pg',
       connection: KnexConfig,
+      pool: { min: 0, max: 7 }
     });
-  }
 }
